d7beb3d7eadc1077a7c957260a86f0eec05f3715
Erik Dittrich <erik.dittrich@gmail.com>
2020-12-30 19:03:13 +0100

    [LP-5] Added rating

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 import random
 
 levels = {
-    "einfach": 65,
-    "normal": 29,
-    "schwer": 26
+    "einfach": 33,
+    "mittel": 29,
+    "schwer": 25
 }
 
 board_00 = [
@@ -31,78 +32,6 @@ board_01 = [
     ]
 
 
-def validate(y, x):
-    n = board[y][x]
-    for i in range(0, 9):
-        if board[y][i] == n and x != i:
-            return False
-
-    for i in range(0, 9):
-        if board[i][x] == n and y != i:
-            return False
-
-    for i in range(y // 3 * 3, y // 3 * 3 + 3):
-        for j in range(x // 3 * 3, x // 3 * 3 + 3):
-            if board[i][j] == n and y != i and x != j:
-                return False
-
-    return True
-
-
-def solve(y, x):
-    if y == 9:
-        return True
-
-    x_new = (x + 1) % 9
-    y_new = (y * 9 + x + 1) // 9
-
-    if board[y][x] > 0:
-        return solve(y_new, x_new)
-    else:
-        for n in range(1, 10):
-            board[y][x] = n
-            if validate(y, x):
-                if solve(y_new, x_new):
-                    return True
-
-        board[y][x] = 0
-        return False
-
-
-def create(y, x):
-    if y == 9:
-        return True
-
-    x_new = (x + 1) % 9
-    y_new = (y * 9 + x + 1) // 9
-
-    list_n = list(range(1, 10))
-
-    for i in range(0, 9):
-        if board[y][i] in list_n:
-            list_n.remove(board[y][i])
-
-    for i in range(0, 9):
-        if board[i][x] in list_n:
-            list_n.remove(board[i][x])
-
-    for i in range(y // 3 * 3, y // 3 * 3 + 3):
-        for j in range(x // 3 * 3, x // 3 * 3 + 3):
-            if board[i][j] in list_n:
-                list_n.remove(board[i][j])
-
-    for i in range(0, len(list_n)):
-        n = list_n[random.randint(0, len(list_n)) - 1]
-        board[y][x] = n
-        list_n.remove(n)
-        if validate(y, x):
-            if create(y_new, x_new):
-                return True
-
-    board[y][x] = 0
-    return False
-
-
 def visualize():
     for y in range(0, 9):
         if y == 0:
@@ -122,9 +51,97 @@ def visualize():
     print('')
 
 
+def validate(y, x):
+    n = board[y][x]
+    for i in range(0, 9):
+        if board[y][i] == n and x != i:
+            return False
+
+    for i in range(0, 9):
+        if board[i][x] == n and y != i:
+            return False
+
+    for i in range(y // 3 * 3, y // 3 * 3 + 3):
+        for j in range(x // 3 * 3, x // 3 * 3 + 3):
+            if board[i][j] == n and y != i and x != j:
+                return False
+
+    return True
+
+
+def create(y=0, x=0):
+    list_n = list(range(1, 10))
+
+    for i in range(0, 9):
+        if board[y][i] in list_n:
+            list_n.remove(board[y][i])
+
+    for i in range(0, 9):
+        if board[i][x] in list_n:
+            list_n.remove(board[i][x])
+
+    for i in range(y // 3 * 3, y // 3 * 3 + 3):
+        for j in range(x // 3 * 3, x // 3 * 3 + 3):
+            if board[i][j] in list_n:
+                list_n.remove(board[i][j])
+
+    x_new = (x + 1) % 9
+    y_new = (y * 9 + x + 1) // 9
+
+    for i in range(0, len(list_n)):
+        n = list_n[random.randint(0, len(list_n)) - 1]
+        board[y][x] = n
+        list_n.remove(n)
+        if y_new == 9 or create(y_new, x_new):
+            return True
+
+    board[y][x] = 0
+    return False
+
+
+def rate(rating):
+    i = 0
+    n = 81 - levels[rating] - random.randint(0, 3)
+
+    while i < n:
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        if board[y][x] != 0:
+            board[y][x] = 0
+            i += 1
+
+
+def solve(y=0, x=0):
+    if y == 9:
+        return True
+
+    x_new = (x + 1) % 9
+    y_new = (y * 9 + x + 1) // 9
+
+    if board[y][x] > 0:
+        return solve(y_new, x_new)
+    else:
+        for n in range(1, 10):
+            board[y][x] = n
+            if validate(y, x):
+                if solve(y_new, x_new):
+                    return True
+
+        board[y][x] = 0
+        return False
+
+
 if __name__ == '__main__':
     board = list(board_00)
-    create(0, 0)
+    t = datetime.now()
+    create()
+    print("Create: ", datetime.now() - t)
     visualize()
-    solve(0, 0)
+    t = datetime.now()
+    rate("schwer")
+    print("Rate:   ", datetime.now() - t)
+    visualize()
+    t = datetime.now()
+    solve()
+    print("Solve:  ", datetime.now() - t)
     visualize()
